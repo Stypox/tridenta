e17322c1b4219eb5ac7c1126092c6bf5eecc77d0
Stypox <stypox@pm.me>
2022-08-23 17:32:26 +0200

    Attach more information to Area enum

--- app/src/main/java/org/stypox/tridenta/data/Area.kt
+++ app/src/main/java/org/stypox/tridenta/data/Area.kt
@@ -1,16 +1,31 @@
 package org.stypox.tridenta.data
 
-enum class Area(val value: Int) {
-    Suburban1(1),
-    Suburban2(2),
-    Suburban3(3),
-    Suburban4(4),
-    Suburban5(5),
-    Suburban6(6),
-    Railway(7),
-    Funicular(8),
-    UrbanPergine(21),
-    UrbanAltoGarda(22),
-    UrbanTrento(23),
-    UrbanRovereto(24),
+import androidx.annotation.ColorInt
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChairAlt
+import androidx.compose.material.icons.filled.Landscape
+import androidx.compose.material.icons.filled.LocationCity
+import androidx.compose.material.icons.filled.Train
+import androidx.compose.ui.graphics.vector.ImageVector
+import org.stypox.tridenta.R
+
+enum class Area(
+    val value: Int,
+    @StringRes val nameRes: Int,
+    @ColorInt val color: Int,
+    val icon: ImageVector,
+) {
+    Suburban1(1, R.string.area_suburban1, 0xc71585, Icons.Filled.Landscape),
+    Suburban2(2, R.string.area_suburban2, 0xb8860b, Icons.Filled.Landscape),
+    Suburban3(3, R.string.area_suburban3, 0x191970, Icons.Filled.Landscape),
+    Suburban4(4, R.string.area_suburban4, 0x228b22, Icons.Filled.Landscape),
+    Suburban5(5, R.string.area_suburban5, 0x7f0000, Icons.Filled.Landscape),
+    Suburban6(6, R.string.area_suburban6, 0x008080, Icons.Filled.Landscape),
+    Railway(7, R.string.area_railway, 0xcc0000, Icons.Filled.Train),
+    Funicular(8, R.string.area_funicular, 0x0000cc, Icons.Filled.ChairAlt),
+    UrbanPergine(21, R.string.area_urban_pergine, 0x000000, Icons.Filled.LocationCity),
+    UrbanAltoGarda(22, R.string.area_urban_alto_garda, 0x000000, Icons.Filled.LocationCity),
+    UrbanTrento(23, R.string.area_urban_trento, 0x000000, Icons.Filled.LocationCity),
+    UrbanRovereto(24, R.string.area_urban_rovereto, 0x000000, Icons.Filled.LocationCity),
 }
--- app/src/main/java/org/stypox/tridenta/ui/line/SuburbanAreasMap.kt
+++ app/src/main/java/org/stypox/tridenta/ui/line/SuburbanAreasMap.kt
@@ -1,10 +1,9 @@
-package org.stypox.tridenta.ui
+package org.stypox.tridenta.ui.line
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.graphics.Color.HSVToColor
 import android.graphics.Color.colorToHSV
-import androidx.annotation.ColorInt
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -22,28 +21,28 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import org.stypox.tridenta.R
+import org.stypox.tridenta.data.Area
 import org.stypox.tridenta.ui.theme.AppTheme
 
-private data class ZoneNumberPosition(
-    val number: Int,
-    @ColorInt val color: Int,
+private data class AreaPosition(
+    val area: Area,
     val x: Float,
     val y: Float,
 )
 
-private val ZONE_NUMBERS = listOf(
-    ZoneNumberPosition(1, 0xc71585, .81f, .21f),
-    ZoneNumberPosition(2, 0xb8860b, .19f, .59f),
-    ZoneNumberPosition(3, 0x191970, .42f, .78f),
-    ZoneNumberPosition(4, 0x228b22, .66f, .50f),
-    ZoneNumberPosition(5, 0x7f0000, .90f, .38f),
-    ZoneNumberPosition(6, 0x008080, .33f, .20f),
+private val AREA_POSITIONS = listOf(
+    AreaPosition(Area.Suburban1, .81f, .21f),
+    AreaPosition(Area.Suburban2, .19f, .59f),
+    AreaPosition(Area.Suburban3, .42f, .78f),
+    AreaPosition(Area.Suburban4, .66f, .50f),
+    AreaPosition(Area.Suburban5, .90f, .38f),
+    AreaPosition(Area.Suburban6, .33f, .20f),
 )
 
 @Composable
-fun SuburbanZonesMap(modifier: Modifier = Modifier, onZoneClick: (Int) -> Unit) {
+fun SuburbanAreasMap(modifier: Modifier = Modifier, onAreaClick: (Area) -> Unit) {
     BoxWithConstraints(modifier = modifier) {
-        val painter = painterResource(id = R.drawable.zones)
+        val painter = painterResource(id = R.drawable.areas)
         // enforce aspect ratio while maximizing width
         val height = this.maxWidth * painter.intrinsicSize.height / painter.intrinsicSize.width
         // the text should be sized with respect to the image size, to avoid overlaps
@@ -57,32 +56,32 @@ fun SuburbanZonesMap(modifier: Modifier = Modifier, onZoneClick: (Int) -> Unit) 
             modifier = Modifier.size(this.maxWidth, height)
         )
 
-        ZONE_NUMBERS.forEach { zoneNumber ->
+        AREA_POSITIONS.forEach { areaPosition ->
             val color = Color(
                 0xff000000 + if (isSystemInDarkTheme()) {
                     val hsvColor = FloatArray(3)
-                    colorToHSV(zoneNumber.color, hsvColor)
+                    colorToHSV(areaPosition.area.color, hsvColor)
                     // increase luminance (i.e. hsvColor[2]) to improve contrast
                     hsvColor[2] = 1.0f - (1.0f - hsvColor[2]) / 3
                     HSVToColor(hsvColor)
                 } else {
-                    zoneNumber.color
+                    areaPosition.area.color
                 }
             )
 
             Text(
-                text = "${zoneNumber.number}",
+                text = "${areaPosition.area.value}",
                 style = MaterialTheme.typography.displayLarge.copy(
                     fontFamily = FontFamily.Monospace,
                     fontSize = textSize,
                     color = color
                 ),
                 modifier = Modifier
-                    .clickable { onZoneClick(zoneNumber.number) }
+                    .clickable { onAreaClick(areaPosition.area) }
                     .align { zone, space, _ ->
                         IntOffset(
-                            (space.width * zoneNumber.x - zone.width / 2).toInt(),
-                            (space.height * zoneNumber.y - zone.height / 2).toInt()
+                            (space.width * areaPosition.x - zone.width / 2).toInt(),
+                            (space.height * areaPosition.y - zone.height / 2).toInt()
                         )
                     }
             )
@@ -91,12 +90,12 @@ fun SuburbanZonesMap(modifier: Modifier = Modifier, onZoneClick: (Int) -> Unit) 
 }
 
 // ensure it scales correctly both at small and high sizes
-@Preview(name = "Suburban zones map, small light", uiMode = UI_MODE_NIGHT_NO, widthDp = 200)
-@Preview(name = "Suburban zones map, medium dark", uiMode = UI_MODE_NIGHT_YES, widthDp = 500)
-@Preview(name = "Suburban zones map, big light", uiMode = UI_MODE_NIGHT_NO, widthDp = 800)
+@Preview(name = "Suburban areas map, small light", uiMode = UI_MODE_NIGHT_NO, widthDp = 200)
+@Preview(name = "Suburban areas map, medium dark", uiMode = UI_MODE_NIGHT_YES, widthDp = 500)
+@Preview(name = "Suburban areas map, big light", uiMode = UI_MODE_NIGHT_NO, widthDp = 800)
 @Composable
-fun SuburbanZonesMapPreview() {
+fun SuburbanAreasMapPreview() {
     AppTheme {
-        SuburbanZonesMap { }
+        SuburbanAreasMap { }
     }
 }
