7c626310aa487797b4c20a04bb95d4ca8a98d4bf
Stypox <stypox@pm.me>
2022-08-26 17:52:35 +0200

    Remove test code from MainActivity

--- app/src/main/java/org/stypox/tridenta/ui/MainActivity.kt
+++ app/src/main/java/org/stypox/tridenta/ui/MainActivity.kt
@@ -1,34 +1,17 @@
 package org.stypox.tridenta.ui
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons.Filled
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import org.stypox.tridenta.R
+import dagger.hilt.android.AndroidEntryPoint
+import org.stypox.tridenta.ui.line.LinesView
 import org.stypox.tridenta.ui.theme.AppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,83 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greetings(List(1000) { Pair("Android $it", "Square ${it*it}") })
+                    LinesView()
                 }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun GreetingsPreview() {
-    AppTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Greetings(List(1000) { Pair("Android $it", "Square ${it * it}") })
-        }
-    }
-}
-
-@Composable
-fun Greetings(namesDescriptions: List<Pair<String, String>>) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier.padding(8.dp)
-    ) {
-        items(items = namesDescriptions) {
-            Greeting(it.first, it.second)
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, description: String) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
-
-    Card {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(12.dp)
-                .animateContentSize(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioMediumBouncy,
-                        stiffness = Spring.StiffnessMediumLow
-                    )
-                )
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(
-                    text = "Hello, $name!",
-                    modifier = Modifier.weight(1f)
-                )
-                IconButton(onClick = { expanded = !expanded }) {
-                    val rotation by animateFloatAsState(
-                        targetValue = if (expanded) 0.0f else 180.0f,
-                        animationSpec = tween()
-                    )
-
-                    Icon(
-                        imageVector = Filled.ExpandMore,
-                        contentDescription = if (expanded) {
-                            stringResource(R.string.show_less)
-                        } else {
-                            stringResource(R.string.show_more)
-                        },
-                        modifier = Modifier.rotate(rotation)
-                    )
-                }
-            }
-            if (expanded) {
-                Text(text = description)
             }
         }
     }
