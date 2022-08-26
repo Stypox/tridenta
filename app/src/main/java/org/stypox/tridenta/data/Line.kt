0d757f65b11260d6961408e714a850c492cf7f32
Stypox <stypox@pm.me>
2022-08-26 20:51:08 +0200

    Sort lines by short name

--- app/src/main/java/org/stypox/tridenta/data/Line.kt
+++ app/src/main/java/org/stypox/tridenta/data/Line.kt
@@ -2,6 +2,8 @@ package org.stypox.tridenta.data
 
 import androidx.annotation.ColorInt
 import java.time.OffsetDateTime
+import java.util.regex.Pattern
+import kotlin.math.min
 
 data class Line(
     val lineId: Int,
@@ -22,3 +24,54 @@ data class NewsItem(
     val url: String,
     val affectedLineIds: List<Int>,
 )
+
+private val SHORT_NAME_SPLIT_PATTERN = Pattern.compile("[^0-9]+|[0-9]+")
+
+private fun splitShortName(shortName: String): ArrayList<Any> {
+    val matcher = SHORT_NAME_SPLIT_PATTERN.matcher(shortName)
+    val result = ArrayList<Any>()
+    while (matcher.find()) {
+        result.add(try {
+            matcher.group().toInt()
+        } catch (e: NumberFormatException) {
+            matcher.group()
+        })
+    }
+    return result
+}
+
+/**
+ * Compares two lines by their short name, such that a list of lines would be sorted
+ * lexicographically except for the fact that numbers are parsed as such and then considered as a
+ * whole (and not as independent chars). So e.g. 2 < 13; A51 < B401; ...
+ */
+fun shortNameComparator(a: Line, b: Line): Int {
+    val aSplit = splitShortName(a.shortName)
+    val bSplit = splitShortName(b.shortName)
+
+    for (i in 0 until min(aSplit.size, bSplit.size)) {
+        if (aSplit[i] is String) {
+            if (bSplit[i] is String) {
+                if (aSplit[i] != bSplit[i]) {
+                    return (aSplit[i] as String).compareTo((bSplit[i] as String))
+                }
+                // else continue to next piece
+            } else /* bSplit[i] is Int */ {
+                return 1 // numbers are before letters
+            }
+
+        } else /* aSplit[i] is Int */ {
+            if (bSplit[i] is String) {
+                return -1 // letters are after numbers
+            } else /* bSplit[i] is Int */ {
+                if (aSplit[i] != bSplit[i]) {
+                    return (aSplit[i] as Int).compareTo((bSplit[i] as Int))
+                }
+                // else continue to next piece
+            }
+        }
+    }
+
+    // the first `min(aSplit.size, bSplit.size)` items are equal, now let's just compare sizes
+    return aSplit.size.compareTo(bSplit.size)
+}
--- app/src/main/java/org/stypox/tridenta/ui/line/LinesViewModel.kt
+++ app/src/main/java/org/stypox/tridenta/ui/line/LinesViewModel.kt
@@ -1,22 +1,18 @@
 package org.stypox.tridenta.ui.line
 
 import android.app.Application
-import android.content.Context
-import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.preference.PreferenceManager
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.stypox.tridenta.TridentaApplication
 import org.stypox.tridenta.data.Area
+import org.stypox.tridenta.data.shortNameComparator
 import org.stypox.tridenta.extractor.Extractor
 import org.stypox.tridenta.ui.pref.PreferenceKeys
 import javax.inject.Inject
@@ -83,6 +79,7 @@ class LinesViewModel @Inject constructor(
         viewModelScope.launch {
             val lines = withContext(Dispatchers.IO) {
                 extractor.getLines(areas = arrayOf(mutableUiState.value.selectedArea))
+                    .sortedWith(::shortNameComparator)
             }
             mutableUiState.update { linesUiState ->
                 linesUiState.copy(lines = lines, loading = false)
