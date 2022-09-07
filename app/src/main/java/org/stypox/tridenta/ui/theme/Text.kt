4556727a3a65794059d0a6eb43bc98df703ab4e2
Stypox <stypox@pm.me>
2022-09-07 09:31:58 +0200

    Unused import

--- app/src/main/java/org/stypox/tridenta/ui/theme/Text.kt
+++ app/src/main/java/org/stypox/tridenta/ui/theme/Text.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
