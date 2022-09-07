ee553f9ad76d283c41b30fa572c19e9a25e190c4
Stypox <stypox@pm.me>
2022-09-07 18:18:07 +0200

    Remove alternatives from StopItem

--- app/src/main/java/org/stypox/tridenta/ui/stops/StopItem.kt
+++ app/src/main/java/org/stypox/tridenta/ui/stops/StopItem.kt
@@ -2,12 +2,10 @@ package org.stypox.tridenta.ui.stops
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,9 +14,9 @@ import org.stypox.tridenta.data.Area
 import org.stypox.tridenta.data.Line
 import org.stypox.tridenta.data.Stop
 import org.stypox.tridenta.data.StopLineType
+import org.stypox.tridenta.ui.lines.LineShortName
 import org.stypox.tridenta.ui.theme.BodyText
 import org.stypox.tridenta.ui.theme.TitleText
-import org.stypox.tridenta.ui.lines.LineShortName
 import org.stypox.tridenta.util.StringUtils.levenshteinDistance
 
 private const val MIN_LEVENSHTEIN_DISTANCE = 3
@@ -32,8 +30,9 @@ fun StopItem(stop: Stop, modifier: Modifier = Modifier) {
             (!streetShown || levenshteinDistance(stop.town, stop.street) > MIN_LEVENSHTEIN_DISTANCE)
     val paddingModifier = modifier.padding(16.dp)
 
-    @Composable
-    fun StopItemDescription() {
+    Column(
+        modifier = paddingModifier
+    ) {
         TitleText(text = stop.name)
         if (streetShown) {
             BodyText(
@@ -49,50 +48,12 @@ fun StopItem(stop: Stop, modifier: Modifier = Modifier) {
                 overflow = TextOverflow.Ellipsis,
             )
         }
-    }
 
-    if (stop.lines.size == 1) {
-        Row(
-            modifier = paddingModifier,
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier.padding(top = 4.dp)
         ) {
-            Column(modifier = Modifier.padding(end = 4.dp).weight(1f)) {
-                StopItemDescription()
-            }
-            LineShortName(line = stop.lines[0])
-        }
-
-    } else if (streetShown && townShown && stop.lines.size == 2) {
-        Row(
-            modifier = paddingModifier,
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            Column(modifier = Modifier.padding(end = 4.dp).weight(1f)) {
-                StopItemDescription()
-            }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(4.dp),
-            ) {
-                LineShortName(line = stop.lines[0])
-                LineShortName(line = stop.lines[1])
-            }
-        }
-
-    } else {
-        Column(
-            modifier = paddingModifier
-        ) {
-            StopItemDescription()
-
-            LazyRow(
-                horizontalArrangement = Arrangement.spacedBy(4.dp),
-                modifier = Modifier.padding(top = 4.dp)
-            ) {
-                items(stop.lines) { line -> LineShortName(line) }
-            }
+            items(stop.lines) { line -> LineShortName(line) }
         }
     }
 }
