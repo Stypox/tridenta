4a0754b278791a820f21db6ff7805a36547a706c
Stypox <stypox@pm.me>
2022-09-08 23:38:28 +0200

    Add support for dynamic theme

--- app/src/main/java/org/stypox/tridenta/ui/theme/Theme.kt
+++ app/src/main/java/org/stypox/tridenta/ui/theme/Theme.kt
@@ -1,10 +1,10 @@
 package org.stypox.tridenta.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 
 private val LightColors = lightColorScheme(
@@ -71,16 +71,19 @@ private val DarkColors = darkColorScheme(
 @Composable
 fun AppTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
+  useDynamicTheme: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
   content: @Composable () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+  val colorScheme =
+      if (useDynamicTheme) {
+          val context = LocalContext.current
+          if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+      } else {
+          if (useDarkTheme) DarkColors else LightColors
+      }
 
   MaterialTheme(
-    colorScheme = colors,
+    colorScheme = colorScheme,
     content = content
   )
 }
