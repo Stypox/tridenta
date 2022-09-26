01a9e4a9775a0d5109a68238e7fb760d4d428c8a
Stypox <stypox@pm.me>
2022-09-26 23:07:21 +0200

    Add LineTripsView based on view model

--- app/src/main/java/org/stypox/tridenta/ui/trips/LineTripsView.kt
+++ app/src/main/java/org/stypox/tridenta/ui/trips/LineTripsView.kt
@@ -1,5 +1,6 @@
 package org.stypox.tridenta.ui.trips
 
+import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -10,25 +11,59 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.android.EntryPointAccessors
 import org.stypox.tridenta.R
 import org.stypox.tridenta.repo.data.UiLine
 import org.stypox.tridenta.repo.data.UiTrip
 import org.stypox.tridenta.sample.SampleUiLineProvider
 import org.stypox.tridenta.sample.SampleUiTripProvider
+import org.stypox.tridenta.ui.MainActivity
 import org.stypox.tridenta.ui.lines.LineShortName
 import org.stypox.tridenta.ui.nav.AppBarDrawerIcon
-import java.time.OffsetDateTime
+import java.time.ZonedDateTime
+
+@Composable
+fun LineTripsView(
+    line: UiLine,
+    navigationIcon: @Composable () -> Unit,
+    lineTripsViewModel: LineTripsViewModel = viewModel(
+        factory = LineTripsViewModel.provideFactory(
+            EntryPointAccessors
+                .fromActivity(
+                    LocalContext.current as Activity,
+                    MainActivity.ViewModelFactoryProvider::class.java
+                )
+                .lineTripsViewModelFactory(),
+            line
+        )
+    )
+) {
+    val lineTripsUiState = lineTripsViewModel.uiState.value
+
+    LineTripsView(
+        line = line,
+        navigationIcon = navigationIcon,
+        setReferenceDateTime = lineTripsViewModel::setReferenceDateTime,
+        trip = lineTripsUiState.trip,
+        prevEnabled = lineTripsUiState.prevEnabled,
+        onPrevClicked = lineTripsViewModel::onPrevClicked,
+        nextEnabled = lineTripsUiState.nextEnabled,
+        onNextClicked = lineTripsViewModel::onNextClicked
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun LineTripsView(
     line: UiLine,
     navigationIcon: @Composable () -> Unit,
-    setReferenceDateTime: (OffsetDateTime) -> Unit,
+    setReferenceDateTime: (ZonedDateTime) -> Unit,
     trip: UiTrip?,
     prevEnabled: Boolean,
     onPrevClicked: () -> Unit,
@@ -65,7 +100,7 @@ private fun LineTripsView(
 private fun LineAppBar(
     line: UiLine,
     navigationIcon: @Composable () -> Unit,
-    setReferenceDateTime: (OffsetDateTime) -> Unit
+    setReferenceDateTime: (ZonedDateTime) -> Unit
 ) {
     // TODO title's width isn't properly calculated when `navigationIcon` and `actions` have
     //  different widths, resulting in the overlap of the `title` and the widest of `actions` or
