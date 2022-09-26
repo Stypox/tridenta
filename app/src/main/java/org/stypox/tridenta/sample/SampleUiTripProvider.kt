1bdfd7e16f2d88f8d1c4aaa422d9ebaa1ad4591f
Stypox <stypox@pm.me>
2022-09-26 14:07:49 +0200

    Create sample provider for UiTrips

---
+++ app/src/main/java/org/stypox/tridenta/sample/SampleUiTripProvider.kt
@@ -0,0 +1,37 @@
+package org.stypox.tridenta.sample
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import org.stypox.tridenta.enums.Direction
+import org.stypox.tridenta.enums.StopLineType
+import org.stypox.tridenta.repo.data.UiStop
+import org.stypox.tridenta.repo.data.UiStopTime
+import org.stypox.tridenta.repo.data.UiTrip
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+class SampleUiTripProvider : PreviewParameterProvider<UiTrip> {
+    private val sampleDbStops = SampleDbStopProvider().values.toList()
+    private val sampleDbLines = SampleDbLineProvider().values.toList()
+    private val referenceDateTime =
+        OffsetDateTime.of(2022, 9, 26, 9, 33, 17, 328943849, ZoneOffset.UTC)
+
+    override val values: Sequence<UiTrip> = sequenceOf(
+        UiTrip(
+            delay = 1,
+            direction = Direction.Backward,
+            lastEventReceivedAt = referenceDateTime.minusMinutes(3),
+            line = sampleDbLines[0],
+            headSign = "Conci \"Villazzano 3\"",
+            tripId = "0003726592022061120220911",
+            type = StopLineType.Urban,
+            completedStops = 2,
+            stopTimes = sampleDbStops.mapIndexed { index, dbStop ->
+                UiStopTime(
+                    arrivalTime = referenceDateTime.plusMinutes((index - 2).toLong()),
+                    departureTime = referenceDateTime.plusMinutes((index + index % 2 - 2).toLong()),
+                    stop = dbStop
+                )
+            }
+        )
+    )
+}
