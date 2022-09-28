56ce9a83b96532181b5e961078ccba2da23decf7
Stypox <stypox@pm.me>
2022-09-28 11:00:25 +0200

    Add method to get ui line from lines repository

--- app/src/main/java/org/stypox/tridenta/repo/LinesRepository.kt
+++ app/src/main/java/org/stypox/tridenta/repo/LinesRepository.kt
@@ -4,6 +4,7 @@ import org.stypox.tridenta.db.LineDao
 import org.stypox.tridenta.db.data.DbLine
 import org.stypox.tridenta.enums.Area
 import org.stypox.tridenta.enums.StopLineType
+import org.stypox.tridenta.repo.data.UiLine
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -21,6 +22,22 @@ class LinesRepository @Inject constructor(
     fun getDbLinesByArea(area: Area): List<DbLine> {
         return stopLineReloadHandler.runAndReloadIfNeeded {
             lineDao.getLinesByArea(area)
+        }
+    }
+
+    fun getUiLine(lineId: Int, lineType: StopLineType): UiLine {
+        return stopLineReloadHandler.runAndReloadIfNeeded {
+            val dbLine = lineDao.getLine(lineId, lineType)
+            val newsItems = lineDao.getNewsForLine(lineId, lineType)
+            UiLine(
+                lineId = dbLine.lineId,
+                type = dbLine.type,
+                area = dbLine.area,
+                color = dbLine.color,
+                longName = dbLine.longName,
+                shortName = dbLine.shortName,
+                newsItems = newsItems
+            )
         }
     }
 }
