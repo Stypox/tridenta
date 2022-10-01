dc85397f211ff56983683895dbefcc1692815db3
Stypox <stypox@pm.me>
2022-10-01 13:35:53 +0200

    Fix missing MeasureFormat class on older android versions

--- app/src/main/java/org/stypox/tridenta/util/FormatUtils.kt
+++ app/src/main/java/org/stypox/tridenta/util/FormatUtils.kt
@@ -1,15 +1,13 @@
 package org.stypox.tridenta.util
 
-import android.icu.text.MeasureFormat
-import android.icu.util.Measure
-import android.icu.util.MeasureUnit
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import org.stypox.tridenta.R
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 import java.time.format.FormatStyle
 import java.time.temporal.ChronoField
-import java.util.*
 
 private val TIME_FORMAT = DateTimeFormatterBuilder()
     .appendValue(ChronoField.HOUR_OF_DAY)
@@ -19,10 +17,9 @@ private val TIME_FORMAT = DateTimeFormatterBuilder()
 
 private val DATE_FORMAT = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
 
+@Composable
 fun formatDurationMinutes(minutes: Int): String {
-    val measureFormat =
-        MeasureFormat.getInstance(Locale.getDefault(), MeasureFormat.FormatWidth.SHORT)
-    return measureFormat.format(Measure(minutes, MeasureUnit.MINUTE))
+    return stringResource(R.string.short_minute_format, minutes)
 }
 
 fun formatTime(offsetDateTime: OffsetDateTime): String {
