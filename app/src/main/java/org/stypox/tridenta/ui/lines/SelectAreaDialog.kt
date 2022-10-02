6589f2535217bc3549aa5d87fcf54d5bfcbfd2fb
Stypox <stypox@pm.me>
2022-10-02 21:16:26 +0200

    Add SelectAreaDialog preview

--- app/src/main/java/org/stypox/tridenta/ui/lines/SelectAreaDialog.kt
+++ app/src/main/java/org/stypox/tridenta/ui/lines/SelectAreaDialog.kt
@@ -9,10 +9,15 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import org.stypox.tridenta.R
@@ -63,5 +68,16 @@ fun SelectAreaDialog(
         properties = DialogProperties(
             usePlatformDefaultWidth = false
         )
+    )
+}
+
+@Preview
+@Composable
+fun SelectAreaDialogPreview() {
+    var selectedArea by rememberSaveable { mutableStateOf(Area.Suburban2) }
+    SelectAreaDialog(
+        selectedArea = selectedArea,
+        setSelectedArea = { selectedArea = it },
+        onDismiss = {}
     )
 }
