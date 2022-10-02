28f6eca1cccb31cb9bdc8f08badebe9cd338e5ea
Stypox <stypox@pm.me>
2022-10-02 21:18:35 +0200

    Use custom dialog for SelectAreaDialog

--- app/src/main/java/org/stypox/tridenta/ui/lines/SelectAreaDialog.kt
+++ app/src/main/java/org/stypox/tridenta/ui/lines/SelectAreaDialog.kt
@@ -5,44 +5,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
-import org.stypox.tridenta.R
+import androidx.compose.ui.window.Dialog
 import org.stypox.tridenta.enums.Area
 
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SelectAreaDialog(
     selectedArea: Area,
     setSelectedArea: (Area) -> Unit,
     onDismiss: () -> Unit
 ) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        confirmButton = {
-            TextButton(onClick = onDismiss) {
-                Text(stringResource(R.string.ok))
-            }
-        },
-        dismissButton = null,
-        text = {
+    Dialog(onDismissRequest = onDismiss) {
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            shape = MaterialTheme.shapes.large
+        ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.verticalScroll(rememberScrollState())
+                modifier = Modifier
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
             ) {
                 val onAreaClick = { area: Area ->
                     setSelectedArea(area)
@@ -53,7 +46,6 @@ fun SelectAreaDialog(
                     onAreaClick = onAreaClick,
                     modifier = Modifier
                         .widthIn(0.dp, 290.dp)
-                        .padding(8.dp)
                 )
 
                 AreaChipGroup(
@@ -64,11 +56,8 @@ fun SelectAreaDialog(
                         .padding(top = 16.dp)
                 )
             }
-        },
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false
-        )
-    )
+        }
+    }
 }
 
 @Preview
