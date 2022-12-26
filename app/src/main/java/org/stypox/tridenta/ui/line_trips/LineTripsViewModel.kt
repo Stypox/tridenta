d011601e53ebd8f52df6677c79ce07aa53cd286c
Stypox <stypox@pm.me>
2022-12-26 19:44:50 +0100

    Prevent concurrently running line trips reload jobs
    Fixes #3

--- app/src/main/java/org/stypox/tridenta/ui/line_trips/LineTripsViewModel.kt
+++ app/src/main/java/org/stypox/tridenta/ui/line_trips/LineTripsViewModel.kt
@@ -5,12 +5,10 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.stypox.tridenta.db.HistoryDao
 import org.stypox.tridenta.extractor.ROME_ZONE_ID
 import org.stypox.tridenta.log.logError
@@ -48,6 +46,8 @@ class LineTripsViewModel @Inject constructor(
     )
     val uiState = mutableUiState.asStateFlow()
 
+    private var tripReloadJob: Job? = null
+
     val isFavorite = historyDao.isFavorite(
         isLine = true,
         id = navArgs.lineId,
@@ -59,7 +59,40 @@ class LineTripsViewModel @Inject constructor(
         setReferenceDateTime(ZonedDateTime.now())
     }
 
+    fun setReferenceDateTime(referenceDateTimeCurrentZone: ZonedDateTime) {
+        cancelTripReloadJobAndLaunch {
+            setReferenceDateTimeAsync(referenceDateTimeCurrentZone)
+        }
+    }
+
+    fun onReload() {
+        cancelTripReloadJobAndLaunch(this::onReloadAsync)
+    }
+
+    fun onPrevClicked() {
+        loadIndex(uiState.value.tripIndex - 1)
+    }
+
+    fun onNextClicked() {
+        loadIndex(uiState.value.tripIndex + 1)
+    }
+
+    fun onFavoriteClicked() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                historyDao.setFavorite(
+                    isLine = true,
+                    id = navArgs.lineId,
+                    type = navArgs.lineType,
+                    isFavorite = isFavorite.value?.not() ?: true
+                )
+            }
+        }
+    }
+
+
     private fun loadLine() {
+        // this job is independent from trip reloading jobs, so don't use cancelReloadJobAndLaunch
         viewModelScope.launch {
             val line = withContext(Dispatchers.IO) {
                 try {
@@ -83,7 +116,18 @@ class LineTripsViewModel @Inject constructor(
         }
     }
 
-    fun setReferenceDateTime(referenceDateTimeCurrentZone: ZonedDateTime) {
+    private fun cancelTripReloadJobAndLaunch(tripLoadingFunction: suspend () -> Unit) {
+        tripReloadJob?.cancel()
+        tripReloadJob = viewModelScope.launch {
+            // clear errors and set the state to "loading" before starting to load
+            mutableUiState.update { it.copy(loading = true, error = false) }
+            tripLoadingFunction()
+            // set "loading" to false when loading finishes (errors are set inside the function)
+            mutableUiState.update { it.copy(loading = false) }
+        }
+    }
+
+    private suspend fun setReferenceDateTimeAsync(referenceDateTimeCurrentZone: ZonedDateTime) {
         val referenceDateTime = referenceDateTimeCurrentZone.withZoneSameInstant(ROME_ZONE_ID)
         mutableUiState.update {
             it.copy(
@@ -93,158 +137,130 @@ class LineTripsViewModel @Inject constructor(
                 prevEnabled = false,
                 nextEnabled = false,
                 referenceDateTime = referenceDateTime,
-                loading = true,
-                error = false,
             )
         }
 
-        viewModelScope.launch {
-            var error = false
-            val (tripsInDayCount, tripIndex, trip) = withContext(Dispatchers.IO) {
-                try {
-                    tripsRepository.getUiTrip(
-                        lineId = navArgs.lineId,
-                        lineType = navArgs.lineType,
-                        referenceDateTime = referenceDateTime
-                    )
-                } catch (e: Throwable) {
-                    logError(
-                        "Could not load trip for UI line (${navArgs.lineId}, " +
-                            "${navArgs.lineType}) at time $referenceDateTime",
-                        e
-                    )
-                    error = true
-                    Triple(0, 0, null)
-                }
-            }
-
-            mutableUiState.update {
-                it.copy(
-                    tripsInDayCount = tripsInDayCount,
-                    tripIndex = tripIndex,
-                    trip = trip,
-                    prevEnabled = tripIndex > 0,
-                    nextEnabled = tripIndex < tripsInDayCount - 1,
-                    referenceDateTime = referenceDateTime,
-                    loading = false,
-                    error = error,
+        var error = false
+        val (tripsInDayCount, tripIndex, trip) = withContext(Dispatchers.IO) {
+            try {
+                tripsRepository.getUiTrip(
+                    lineId = navArgs.lineId,
+                    lineType = navArgs.lineType,
+                    referenceDateTime = referenceDateTime
                 )
+            } catch (e: Throwable) {
+                logError(
+                    "Could not load trip for UI line (${navArgs.lineId}, " +
+                            "${navArgs.lineType}) at time $referenceDateTime",
+                    e
+                )
+                error = true
+                Triple(0, 0, null)
             }
+        }
+
+        mutableUiState.update {
+            it.copy(
+                tripsInDayCount = tripsInDayCount,
+                tripIndex = tripIndex,
+                trip = trip,
+                prevEnabled = tripIndex > 0,
+                nextEnabled = tripIndex < tripsInDayCount - 1,
+                referenceDateTime = referenceDateTime,
+                error = error,
+            )
         }
     }
 
-    fun onReload() {
+    private suspend fun onReloadAsync() {
         val previousTrip = uiState.value.trip
         if (previousTrip == null) {
             // this could happen if an error happened while loading initial/more trips
             if (uiState.value.tripsInDayCount > 0) {
                 // more trips failed loading, try to load again the currently set index
-                loadIndex(uiState.value.tripIndex)
+                loadIndexAsync(uiState.value.tripIndex)
             } else {
                 // initial trips failed loading, try to load again the current day
-                setReferenceDateTime(uiState.value.referenceDateTime)
+                setReferenceDateTimeAsync(uiState.value.referenceDateTime)
             }
             return
         }
 
-        mutableUiState.update { it.copy(loading = true, error = false) }
-        viewModelScope.launch {
-            val trip = withContext(Dispatchers.IO) {
-                try {
-                    tripsRepository.reloadUiTrip(
-                        uiTrip = previousTrip,
-                        index = uiState.value.tripIndex,
-                        referenceDateTime = uiState.value.referenceDateTime
-                    )
-                } catch (e: Throwable) {
-                    logError(
-                        "Could not load trip ${previousTrip.tripId} for UI line " +
-                                "(${navArgs.lineId}, ${navArgs.lineType})",
-                        e
-                    )
-                    null
-                }
-            }
-
-            if (trip == null) {
-                // keep previous trip intact, we don't want to hide information that we do have!
-                mutableUiState.update { it.copy(loading = false, error = true) }
-            } else {
-                mutableUiState.update { it.copy(trip = trip, loading = false, error = false) }
+        val trip = withContext(Dispatchers.IO) {
+            try {
+                tripsRepository.reloadUiTrip(
+                    uiTrip = previousTrip,
+                    index = uiState.value.tripIndex,
+                    referenceDateTime = uiState.value.referenceDateTime
+                )
+            } catch (e: Throwable) {
+                logError(
+                    "Could not load trip ${previousTrip.tripId} for UI line " +
+                            "(${navArgs.lineId}, ${navArgs.lineType})",
+                    e
+                )
+                null
             }
         }
-    }
 
-    fun onPrevClicked() {
-        loadIndex(uiState.value.tripIndex - 1)
-    }
-
-    fun onNextClicked() {
-        loadIndex(uiState.value.tripIndex + 1)
+        if (trip == null) {
+            // keep previous trip intact, we don't want to hide information that we do have!
+            mutableUiState.update { it.copy(error = true) }
+        } else {
+            mutableUiState.update { it.copy(trip = trip) }
+        }
     }
 
     private fun loadIndex(index: Int) {
-        if (index < 0 || index >= uiState.value.tripsInDayCount) {
-            return // this will happen when there are no trips in a day, for example
+        if (index >= 0 && index < uiState.value.tripsInDayCount) {
+            // only cancel any currently running job if there is something to do; the above
+            // condition will be false e.g. when there are no trips in a day (but not only for that)
+            cancelTripReloadJobAndLaunch {
+                loadIndexAsync(index)
+            }
         }
+    }
 
+    private suspend fun loadIndexAsync(index: Int) {
         mutableUiState.update {
             it.copy(
                 tripIndex = index,
                 trip = null,
                 prevEnabled = index > 0,
                 nextEnabled = index < uiState.value.tripsInDayCount - 1,
-                loading = true,
-                error = false,
             )
         }
 
-        viewModelScope.launch {
-            val (trip, loadedFromNetwork) = withContext(Dispatchers.IO) {
-                try {
-                    tripsRepository.getUiTrip(
-                        lineId = navArgs.lineId,
-                        lineType = navArgs.lineType,
-                        referenceDateTime = uiState.value.referenceDateTime,
-                        index = index
-                    )
-                } catch (e: Throwable) {
-                    logError(
-                        "Could not load trip at index $index for UI line " +
-                                "(${navArgs.lineId}, ${navArgs.lineType})",
-                        e
-                    )
-                    Pair(null, true /* <- useless when trip == null */)
-                }
-            }
-
-            // show the trip even if loadedFromNetwork is false (in which case it could be outdated)
-            mutableUiState.update {
-                it.copy(
-                    trip = trip,
-                    loading = false,
-                    error = trip == null,
+        val (trip, loadedFromNetwork) = withContext(Dispatchers.IO) {
+            try {
+                tripsRepository.getUiTrip(
+                    lineId = navArgs.lineId,
+                    lineType = navArgs.lineType,
+                    referenceDateTime = uiState.value.referenceDateTime,
+                    index = index
                 )
-            }
-
-            if (!loadedFromNetwork && trip != null && trip.completedStops < trip.stopTimes.size) {
-                // after showing the (possibly) outdated trip fast, reload it to show latest updates
-                // (but reload it only if there actually is a trip and it is not completed)
-                onReload()
+            } catch (e: Throwable) {
+                logError(
+                    "Could not load trip at index $index for UI line " +
+                            "(${navArgs.lineId}, ${navArgs.lineType})",
+                    e
+                )
+                Pair(null, true /* <- useless when trip == null */)
             }
         }
-    }
 
-    fun onFavoriteClicked() {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                historyDao.setFavorite(
-                    isLine = true,
-                    id = navArgs.lineId,
-                    type = navArgs.lineType,
-                    isFavorite = isFavorite.value?.not() ?: true
-                )
-            }
+        // show the trip even if loadedFromNetwork is false (in which case it could be outdated)
+        mutableUiState.update {
+            it.copy(
+                trip = trip,
+                error = trip == null,
+            )
+        }
+
+        if (!loadedFromNetwork && trip != null && trip.completedStops < trip.stopTimes.size) {
+            // after showing the (possibly) outdated trip fast, reload it to show latest updates
+            // (but reload it only if there actually is a trip and it is not completed)
+            onReloadAsync()
         }
     }
 }
