76916e816bccc09660f0ac0b39c2c50f401fc356
Stypox <stypox@pm.me>
2025-02-21 20:26:01 +0100

    Fix SmallCircularProgressIndicator color

--- app/src/main/java/org/stypox/tridenta/ui/theme/SmallCircularProgressIndicator.kt
+++ app/src/main/java/org/stypox/tridenta/ui/theme/SmallCircularProgressIndicator.kt
@@ -2,6 +2,7 @@ package org.stypox.tridenta.ui.theme
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,7 +12,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun SmallCircularProgressIndicator(modifier: Modifier = Modifier) {
     CircularProgressIndicator(
+        strokeWidth = 2.dp,
+        color = LocalContentColor.current,
         modifier = modifier.size(16.dp),
-        strokeWidth = 2.dp
     )
 }
